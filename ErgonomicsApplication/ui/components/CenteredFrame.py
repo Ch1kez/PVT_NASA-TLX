4f142af10c849eb826fb18e01fb34ea492081e9b
ch1kez <danila421@mail.ru>
2024-12-26 03:28:23 +0300

    123456789

--- ErgonomicsApplication/ui/components/CenteredFrame.py
+++ ErgonomicsApplication/ui/components/CenteredFrame.py
@@ -14,18 +14,18 @@ class CenteredFrame(tk.Frame):
 
         # Пример логотипов (если есть такие файлы)
         # Пути можно подкорректировать, чтобы они соответствовали вашим реальным asset-ам
-        self.left_logo_path = "ui/assets/logo.png"
-        self.right_logo_path = "ui/assets/logo.png"
+        self.left_logo_path = "ui/assets/mai_logo.png"
+        # self.right_logo_path = "ui/assets/mai_logo.png"
 
         try:
             self.left_logo = ImageTk.PhotoImage(Image.open(self.left_logo_path).resize((80, 80), Image.Resampling.LANCZOS))
-            self.right_logo = ImageTk.PhotoImage(Image.open(self.right_logo_path).resize((80, 80), Image.Resampling.LANCZOS))
+            # self.right_logo = ImageTk.PhotoImage(Image.open(self.right_logo_path).resize((80, 80), Image.Resampling.LANCZOS))
 
             self.left_logo_label = tk.Label(self, image=self.left_logo, bg="#f7f7f7")
             self.left_logo_label.place(relx=1.0, x=-10, y=10, anchor="ne")
 
-            self.right_logo_label = tk.Label(self, image=self.right_logo, bg="#f7f7f7")
-            self.right_logo_label.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-10)
+            # self.right_logo_label = tk.Label(self, image=self.right_logo, bg="#f7f7f7")
+            # self.right_logo_label.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-10)
         except:
             pass  # если нет логотипов, игнорируем
 
